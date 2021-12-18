f2754e4d5e951453c276ab3094c64ac6895cb8e6
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-12-18 10:48:39 +0530

    Update and delete product- API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,3 +28,56 @@ exports.getAllProducts = async (req, res) => {
     res.status(400).send();
   }
 };
+
+//update product --ADMIN
+exports.updateProduct = async (req, res, next) => {
+  try {
+    let toBeUpdated = await Product.findById(req.params.id);
+
+    if (!toBeUpdated) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    toBeUpdated = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      toBeUpdated,
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    res.status(400).send(error.message);
+  }
+};
+
+//delete a product - ADMIN
+
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const toBeDeleted = await Product.findById(req.params.id);
+
+    if (!toBeDeleted) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    await toBeDeleted.remove();
+
+    res.status(200).json({
+      success: true,
+      message: "Product Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    res.status(400).send(error.message);
+  }
+};
