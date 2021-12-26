dd2f278a72ca264e052aa0b1ec69c54234fcbb26
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-12-26 21:24:49 +0530

    jwttoken.js- for generating token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middleware/catchAsyncError");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwttoken");
 
 //Register
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -16,10 +17,28 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
     },
   });
 
-  const token = user.getJWTToken();
+  sendToken(user, 201, res);
+});
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
+//Log In
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Enter email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  sendToken(user, 200, res);
 });
