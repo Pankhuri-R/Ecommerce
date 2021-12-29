0cb44ec822f1d383516ea3d30e26cb15e71a4fdd
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-12-29 21:47:46 +0530

    send email js file

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,18 @@
+const nodeMailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    service: process.env.SMPT_SERVICE,
+    auth: {
+      user: process.env.SMPT_EMAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_EMAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+};
